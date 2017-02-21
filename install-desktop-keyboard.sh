4f7b7d517584c2ec55c84b3c9800b4d15e05d62c
taringamberini <taringamberini@users.noreply.github.com>
2017-02-21 23:29:23 +0100

    improve installation

--- install-desktop-keyboard.sh
+++ install-desktop-keyboard.sh
@@ -2,24 +2,27 @@
 
 INSTALL_DIR=xmodmap-my-conf
 DATE=$(date +%Y%m%d%H%M%S)
-echo "Installing xmodmap configuration in ~/$INSTALL_DIR"
+BACKUP_DIR="$INSTALL_DIR-backup-$DATE"
+
+
+echo -n "Backing up previous xmodmap keycode and keysyms in ~/xmodmap-pke-original ... "
+mkdir ~/$BACKUP_DIR
+xmodmap -pke >  ~/$BACKUP_DIR/xmodmap-pke
+echo "success"
+echo "To restore backed up xmodmap keycode and keysyms run: xmodmap ~/xmodmap-pke-original"
 
 echo "Checking for previous xmodmap configuration"
 if [ -f ~/.xmodmaprc ] || [ -d ~/$INSTALL_DIR ]
 then
 	echo "You already have an existing xmodmap configuration"
-	BACKUP_DIR="xmodmap-configuration-backup-$DATE"
 	echo -n "Trying to back it up in ~/$BACKUP_DIR ... "
-	mkdir ~/$BACKUP_DIR
 	mv ~/.xmodmaprc ~/$BACKUP_DIR
 	mv ~/$INSTALL_DIR ~/$BACKUP_DIR
 	echo "success"
 fi
 
-echo -n "Backing up previous xmodmap keycode and keysyms in ~/xmodmap-pke-original ... "
-xmodmap -pke > ~/xmodmap-pke-original-$DATE
-echo "success"
-echo "To restore backed up xmodmap keycode and keysyms run: xmodmap ~/xmodmap-pke-original"
+
+echo "Installing xmodmap desktop configuration in ~/$INSTALL_DIR"
 
 echo "Cloning my xmodmap configuration from GitHub in ~/$INSTALL_DIR"
 git clone https://github.com/taringamberini/my-xmodmap-configuration.git ~/$INSTALL_DIR
@@ -28,5 +31,5 @@ echo -n "Creating symlink ... "
 ln -s ~/$INSTALL_DIR/xmodmaprc-desktop-keyboard ~/.xmodmaprc
 echo "success"
 
-echo "xmodmap installation completed :-)"
+echo "xmodmap desktop installation completed :-)"
 
--- install-laptop-keyboard.sh
+++ install-laptop-keyboard.sh
@@ -2,31 +2,34 @@
 
 INSTALL_DIR=xmodmap-my-conf
 DATE=$(date +%Y%m%d%H%M%S)
-echo "Installing xmodmap configuration in ~/$INSTALL_DIR"
+BACKUP_DIR="$INSTALL_DIR-backup-$DATE"
+
+
+echo -n "Backing up previous xmodmap keycode and keysyms in ~/xmodmap-pke-original ... "
+mkdir ~/$BACKUP_DIR
+xmodmap -pke >  ~/$BACKUP_DIR/xmodmap-pke
+echo "success"
+echo "To restore backed up xmodmap keycode and keysyms run: xmodmap ~/xmodmap-pke-original"
 
 echo "Checking for previous xmodmap configuration"
 if [ -f ~/.xmodmaprc ] || [ -d ~/$INSTALL_DIR ]
 then
 	echo "You already have an existing xmodmap configuration"
-	BACKUP_DIR="xmodmap-configuration-backup-$DATE"
 	echo -n "Trying to back it up in ~/$BACKUP_DIR ... "
-	mkdir ~/$BACKUP_DIR
 	mv ~/.xmodmaprc ~/$BACKUP_DIR
 	mv ~/$INSTALL_DIR ~/$BACKUP_DIR
 	echo "success"
 fi
 
-echo -n "Backing up previous xmodmap keycode and keysyms in ~/xmodmap-pke-original ... "
-xmodmap -pke > ~/xmodmap-pke-original-$DATE
-echo "success"
-echo "To restore backed up xmodmap keycode and keysyms run: xmodmap ~/xmodmap-pke-original"
 
-echo "Cloning my xmodmap configuration from GitHub in ~/$INSTALL_DIR"
+echo "Installing xmodmap configuration in ~/$INSTALL_DIR"
+
+echo "Cloning my xmodmap laptop configuration from GitHub in ~/$INSTALL_DIR"
 git clone https://github.com/taringamberini/my-xmodmap-configuration.git ~/$INSTALL_DIR
 
 echo -n "Creating symlink ... "
 ln -s ~/$INSTALL_DIR/xmodmaprc-laptop-keyboard ~/.xmodmaprc
 echo "success"
 
-echo "xmodmap installation completed :-)"
+echo "xmodmap laptop installation completed :-)"
 
